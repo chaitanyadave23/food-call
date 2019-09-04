3d88a55c25eb538cebe06a95769416e5e6a22950
Chaitanya <chaitanya@example.com>
2019-09-04 09:14:19 +0530

    commiting

--- user.php
+++ user.php
@@ -7,46 +7,53 @@
   <title>Order Here</title>
   <link rel="stylesheet" href="css/card.css">
   <link rel="stylesheet" href="css/button.css">
+  <link rel="stylesheet" href="css/cart.css">
 </head>
 <body>
 	<div id="maincontent">
 		<?php
 
 		$result = mysqli_query($con,"SELECT * FROM main_menu");
-
+		$i=0;
 		while($row = mysqli_fetch_array($result))
 		{
-		    
 		    echo"
-
 			<div class='card'>
-
+		
 			  <img src='images/home.jpg' alt='Avatar' style='width:100%'>
-			  
+			
 			  <div class='container'>
-			    <h4><b>{$row['name']}</b></h4> 
+
+			    <h4><b><p id='n".$i."'>{$row['name']}</p></b></h4> 
 			    <p>Architect & Engineer</p> 
 			  </div>
 			  <div>
-			  	<button class='button'>Add</button>
+			  	<button class='button' value='n".$i."'onclick='add(this)'>Add</button>
 			  </div>
 			</div>";
-
+			$i+=1;
 		}
+		?>	
 
+			<div id="cart">Your Orders</div>
+			</div>
 
+		<script>
+			var na;
+			var iter;
+			function add(element){
+				iter=element.value;
+				na = document.getElementById(iter).textContent;
+				// document.getElementById("cart").innerHTML=na;
 
-
-
-		
-		?> 			  			  
+				var node = document.createElement("p");                 
+				var textnode = document.createTextNode(na);         
+				node.appendChild(textnode);                         
+				document.getElementById("cart").appendChild(node);
+			}
+		</script>
 </body>
 </html>
-
-
-
-
-
 <?php
         
 
